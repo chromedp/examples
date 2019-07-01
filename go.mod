9a02a61d8242baaf975aa5d0e71ed1a4fd9a3800
Kenneth Shaw <kenneth.shaw@knq.io>
2019-07-01 08:35:14 +0700

    Updating screenshot with full page capture example.

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/chromedp/examples
 go 1.11
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20190614062957-d6d2f92b486d
-	github.com/chromedp/chromedp v0.3.1-0.20190617065505-d55cf9043e05
+	github.com/chromedp/cdproto v0.0.0-20190627233448-54eaa9e8c6aa
+	github.com/chromedp/chromedp v0.3.1
 	github.com/gobwas/ws v1.0.2 // indirect
-	github.com/mailru/easyjson v0.0.0-20190614124828-94de47d64c63 // indirect
-	golang.org/x/sys v0.0.0-20190618155005-516e3c20635f // indirect
+	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
 )
--- screenshot/main.go
+++ screenshot/main.go
@@ -6,7 +6,10 @@ import (
 	"context"
 	"io/ioutil"
 	"log"
+	"math"
 
+	"github.com/chromedp/cdproto/emulation"
+	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 )
 
@@ -15,23 +18,75 @@ func main() {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	// run task list
+	// capture screenshot of an element
 	var buf []byte
-	err := chromedp.Run(ctx, screenshot(`https://www.google.com/`, `#main`, &buf))
-	if err != nil {
+	if err := chromedp.Run(ctx, elementScreenshot(`https://www.google.com/`, `#main`, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile("elementScreenshot.png", buf, 0644); err != nil {
 		log.Fatal(err)
 	}
 
-	// save the screenshot to disk
-	if err = ioutil.WriteFile("screenshot.png", buf, 0644); err != nil {
+	// capture entire browser viewport, returning png with quality=90
+	if err := chromedp.Run(ctx, fullScreenshot(`https://brank.as/`, 90, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile("fullScreenshot.png", buf, 0644); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func screenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
+// elementScreenshot takes a screenshot of a specific element.
+func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(urlstr),
 		chromedp.WaitVisible(sel, chromedp.ByID),
 		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
+	}
+}
+
+// fullScreenshot takes a screenshot of the entire browser viewport.
+//
+// Liberally copied from puppeteer's source.
+//
+// Note: this will override the viewport emulation settings.
+func fullScreenshot(urlstr string, quality int64, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// get layout metrics
+			_, _, contentSize, err := page.GetLayoutMetrics().Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			width, height := int64(math.Ceil(contentSize.Width)), int64(math.Ceil(contentSize.Height))
+
+			// force viewport emulation
+			err = emulation.SetDeviceMetricsOverride(width, height, 1, false).
+				WithScreenOrientation(&emulation.ScreenOrientation{
+					Type:  emulation.OrientationTypePortraitPrimary,
+					Angle: 0,
+				}).
+				Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			// capture screenshot
+			*res, err = page.CaptureScreenshot().
+				WithQuality(quality).
+				WithClip(&page.Viewport{
+					X:      contentSize.X,
+					Y:      contentSize.Y,
+					Width:  contentSize.Width,
+					Height: contentSize.Height,
+					Scale:  1,
+				}).Do(ctx)
+			if err != nil {
+				return err
+			}
+			return nil
+		}),
 	}
 }
