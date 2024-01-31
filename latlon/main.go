e17f8bc1b5fdba5827d72d5d6d409381e5819975
Kenneth Shaw <kenshaw@gmail.com>
2024-02-01 05:57:10 +0700

    Quick cleanup to latlon example

--- latlon/main.go
+++ latlon/main.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"time"
@@ -15,20 +16,25 @@ import (
 )
 
 func main() {
-	timeout := flag.Duration("timeout", 30*time.Second, "timeout")
+	verbose := flag.Bool("v", false, "verbose")
+	timeout := flag.Duration("timeout", 1*time.Minute, "timeout")
 	flag.Parse()
-	if err := run(context.Background(), *timeout); err != nil {
+	if err := run(context.Background(), *verbose, *timeout); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, timeout time.Duration) error {
-	// create regexp to extract latitude, longitude from the url
+func run(ctx context.Context, verbose bool, timeout time.Duration) error {
+	// regexp to extract latitude, longitude
 	latlonRE := regexp.MustCompile(`maps/@(-?\d+\.\d+,-?\d+\.\d+),`)
 
 	// create chrome instance
-	ctx, cancel := chromedp.NewContext(ctx /*, chromedp.WithDebugf(log.Printf)*/)
+	var opts []chromedp.ContextOption
+	if verbose {
+		opts = append(opts, chromedp.WithDebugf(log.Printf))
+	}
+	ctx, cancel := chromedp.NewContext(ctx, opts...)
 	defer cancel()
 
 	// create a timeout
@@ -37,9 +43,10 @@ func run(ctx context.Context, timeout time.Duration) error {
 
 	// listen for the navigated event
 	ch, errch := make(chan string, 1), make(chan error, 1)
-	defer close(ch)
-	defer close(errch)
 	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		if verbose {
+			log.Printf("%T: %+v\n", ev, ev)
+		}
 		switch event := ev.(type) {
 		case *page.EventNavigatedWithinDocument:
 			if m := latlonRE.FindStringSubmatch(event.URL); m != nil {
@@ -47,18 +54,22 @@ func run(ctx context.Context, timeout time.Duration) error {
 			}
 		}
 	})
+
+	// run task
 	go func() {
 		if err := chromedp.Run(ctx, chromedp.Navigate("https://www.google.com/maps/?hl=en")); err != nil {
 			errch <- err
 		}
 	}()
+
+	// wait for context closed, an error, or the result
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
 	case err := <-errch:
 		return err
-	case urlstr := <-ch:
-		fmt.Fprintln(os.Stdout, urlstr)
+	case res := <-ch:
+		fmt.Fprintln(os.Stdout, res)
 	}
 	return nil
 }
